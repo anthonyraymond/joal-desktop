0c87cdfe598d4430869b8ac68edb69334efb299c
anthonyraymond <anthonyraymond@users.noreply.github.com>
2017-08-19 23:40:00 +0200

    add some text on initializeStatus component

--- app/components/initializeStatus/initializeStatus.component.js
+++ app/components/initializeStatus/initializeStatus.component.js
@@ -6,9 +6,13 @@ import JOAL from './joal';
 const InitializeStatus = (props) => {
   return (
     <div>
-      <h1>Jre</h1>
+      <div>
+        In order to work JOAL need to download some dependencies, hang tight.
+        Do not close the app before downloads ends as it could mess the build.
+      </div>
+      <h3>Embeded Java</h3>
       <JRE />
-      <h1>Joal</h1>
+      <h3>Joal-core</h3>
       <JOAL />
     </div>
   );
