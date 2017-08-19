6f66be62bf1ee6ce7582a4e47d4b47f2b54a08f5
anthonyraymond <anthonyraymond@users.noreply.github.com>
2017-08-20 00:02:03 +0200

    Fix linter error in initializeStatus component

--- app/components/initializeStatus/initializeStatus.component.js
+++ app/components/initializeStatus/initializeStatus.component.js
@@ -3,19 +3,17 @@ import React from 'react';
 import JRE from './jre';
 import JOAL from './joal';
 
-const InitializeStatus = (props) => {
-  return (
+const InitializeStatus = () => (
+  <div>
     <div>
-      <div>
-        In order to work JOAL need to download some dependencies, hang tight.
-        Do not close the app before downloads ends as it could mess the build.
-      </div>
-      <h3>Embeded Java</h3>
-      <JRE />
-      <h3>Joal-core</h3>
-      <JOAL />
+      In order to work JOAL need to download some dependencies, hang tight.
+      Do not close the app before downloads ends as it could mess the build.
     </div>
-  );
-};
+    <h3>Embeded Java</h3>
+    <JRE />
+    <h3>Joal-core</h3>
+    <JOAL />
+  </div>
+);
 
 export default InitializeStatus;
